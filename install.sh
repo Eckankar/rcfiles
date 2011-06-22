c3c1669888619a29c46ba416b25bb79513d03aed
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2011-06-22 22:42:43 +0200

    Add -T to ln calls in install.sh
    Otherwise, doing a ln for an already installed folder turns ugly.
    Thanks to kristoff3r.

--- install.sh
+++ install.sh
@@ -3,30 +3,30 @@
 DIR=`pwd`
 
 echo Installing vim config...
-ln -fs $DIR/vimrc $HOME/.vimrc
-ln -fs $DIR/vim $HOME/.vim
+ln -Tfs $DIR/vimrc $HOME/.vimrc
+ln -Tfs $DIR/vim $HOME/.vim
 mkdir -p /var/tmp/vim-sebbe/swap
 mkdir -p /var/tmp/vim-sebbe/undo
 
 echo Installing zsh config...
-ln -fs $DIR/zshcomplete $HOME/.zshcomplete
-ln -fs $DIR/zshrc $HOME/.zshrc
-ln -fs $DIR/zshenv $HOME/.zshenv
+ln -Tfs $DIR/zshcomplete $HOME/.zshcomplete
+ln -Tfs $DIR/zshrc $HOME/.zshrc
+ln -Tfs $DIR/zshenv $HOME/.zshenv
 touch $HOME/.zshenv.local
 echo Setting default shell to zsh...
 chsh -s `which zsh`
 
 echo Installing git config...
-ln -fs $DIR/gitconfig $HOME/.gitconfig
+ln -Tfs $DIR/gitconfig $HOME/.gitconfig
 
 echo Installing screen config...
-ln -fs $DIR/screenrc $HOME/.screenrc
+ln -Tfs $DIR/screenrc $HOME/.screenrc
 
 echo Installing irssi config...
-ln -fs $DIR/irssi $HOME/.irssi
+ln -Tfs $DIR/irssi $HOME/.irssi
 
 echo Installing ack config...
-ln -fs $DIR/ackrc $HOME/.ackrc
+ln -Tfs $DIR/ackrc $HOME/.ackrc
 
 echo All done!
 
